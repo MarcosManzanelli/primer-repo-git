88c6d7486f7d4723b9c6c936ed38c4c3f4d6c463
MarcosManzanelli <marcos.manzanelli.l@gmail.com>
2025-03-25 19:22:48 -0300

    Se agregaron comentarios a ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,7 @@
-# Se definen las variables 
+# Se definen las variables nombre
 nombre = input("¿Cuál es tu nombre? ")
+# Se definen las variables apellido
 apellido = input("¿Cuál es tu apellido? ")
+# Se definen las variables materia
 materia = input("¿Cuál es tu materia? ")
 print(f"Hola soy {nombre} {apellido} alumno de {materia}.")
