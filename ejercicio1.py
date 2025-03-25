f6e417781ce0d668f1efc341945369a0cb4f498d
MarcosManzanelli <marcos.manzanelli.l@gmail.com>
2025-03-25 19:15:22 -0300

    Se agregaron comentarios a ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,5 @@
-nombre = "Marcos"
-apellido = "Manzanelli"
-materia = "Programación 1"
+# Se definen las variables 
+nombre = input("¿Cuál es tu nombre? ")
+apellido = input("¿Cuál es tu apellido? ")
+materia = input("¿Cuál es tu materia? ")
 print(f"Hola soy {nombre} {apellido} alumno de {materia}.")
