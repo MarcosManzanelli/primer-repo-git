b1051722e97b6542930ac1e2702074b9e47c6abd
MarcosManzanelli <marcos.manzanelli.l@gmail.com>
2025-03-25 19:05:58 -0300

    Se crea ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,4 @@
+nombre = input("¿Cuál es tu nombre? ")
+apellido = input("¿Cuál es tu apellido? ")
+materia = input("¿Cuál es tu materia? ")
+print(f"Hola soy {nombre} {apellido} alumno de {materia}.")
